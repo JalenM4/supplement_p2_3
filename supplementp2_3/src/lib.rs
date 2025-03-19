1ea6811af7e31911c49f0fac62cd215304ab150c
jalen4 <jmas0418@students.vsu.edu>
2025-03-19 14:03:42 -0400

    documentation: added documentation for add heap and add mixed

--- supplementp2_3/src/lib.rs
+++ supplementp2_3/src/lib.rs
@@ -9,10 +9,24 @@ fn add_stack(a: i32,b: i32) -> i32 {
     a + b
 }
 
+/// # Description 
+/// Function to add two numbers allocated on the heap
+/// # Parameters
+/// a - 32 bit integer stored in the heap
+/// b - 32 bit integer stored in the heap
+/// # Returns
+/// returnd the sum of the added heap
 fn add_heap(a: Box<i32>, b: Box<i32>) -> i32 {
     *a + *b
 }
 
+/// # Description 
+/// Function to add a stack-allocated number to a heap-allocated number
+/// # Parameters 
+/// stack_num - a integer allocated on the stack
+/// heap_num - a heap allocated integer
+/// # Returns 
+/// returns a single integer, which is the sum of the stack-allocated number and the dereferenced heap-allocated number
 fn add_mixed(stack_num: i32, heap_num: Box<i32>) -> i32 {
     stack_num + *heap_num
 }
