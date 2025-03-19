25f76e354d08874f9ac6b86109d1d079806c18a3
jalen4 <jmas0418@students.vsu.edu>
2025-03-19 13:54:43 -0400

    added test for add heap and add mixed

--- supplementp2_3/src/test.rs
+++ supplementp2_3/src/test.rs
@@ -7,4 +7,14 @@ mod tests {
     fn test_add_stack() {
         assert_eq!(add_stack(3, 7), 10);
     }
+
+    #[test]
+    fn test_add_heap() {
+        assert_eq!(add_heap(Box::new(5), Box::new(8)), 13);
+    }
+
+    #[test]
+    fn test_add_mixed() {
+        assert_eq!(add_mixed(4, Box::new(6)), 10);
+    }
 }
