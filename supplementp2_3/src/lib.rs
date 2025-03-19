f9e9ee030c5db255566a7471e5fc71a45c25b694
jalen4 <jmas0418@students.vsu.edu>
2025-03-19 13:59:19 -0400

    implemented add_heap and add_mixed function

--- supplementp2_3/src/lib.rs
+++ supplementp2_3/src/lib.rs
@@ -10,11 +10,11 @@ fn add_stack(a: i32,b: i32) -> i32 {
 }
 
 fn add_heap(a: Box<i32>, b: Box<i32>) -> i32 {
-    todo!("not implemented!")
+    *a + *b
 }
 
 fn add_mixed(stack_num: i32, heap_num: Box<i32>) -> i32 {
-    todo!("not implemented")
+    stack_num + *heap_num
 }
 
 mod test;
