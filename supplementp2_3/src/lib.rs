9fe7b26c578c28a718139b807f1b724677b9c2cd
jalen4 <jmas0418@students.vsu.edu>
2025-03-19 13:49:21 -0400

    documentation: added documentation for add stack

--- supplementp2_3/src/lib.rs
+++ supplementp2_3/src/lib.rs
@@ -1,3 +1,10 @@
+/// # Description 
+/// This function adds two numbers allocated on the stack
+/// # Parameters
+/// a - 32-bit integer stored on the stack
+/// b - 32-bit integer stored on the stack.
+/// # Returns
+/// returns the sum of two allocated integers
 fn add_stack(a: i32,b: i32) -> i32 {
     a + b
 }
